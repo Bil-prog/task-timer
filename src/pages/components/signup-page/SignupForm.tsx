050781a1629d2867c4d35e2ab32d0a158ed721f2
Bilha Mwengi <bilhamwengi34@gmail.com>
2022-06-15 22:29:02 +0300

    JSON Web Token

--- src/pages/components/signup-page/SignupForm.tsx
+++ src/pages/components/signup-page/SignupForm.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
+import { UserContext } from "../../contexts/UserContext"
+import { useRouter } from "next/router"
+import { harperFetchJWTTokens } from "../../utils/harperdb/fetchJWTTokens"
 import { LabelAndInput } from "../Form"
 import Button from "../Button"
 import { postFormData } from "../../utils/postFormData"
@@ -10,6 +13,8 @@ const SignupForm = () => {
   const [password1, setPassword1] = useState("")
   const [password2, setPassword2] = useState("")
   const [errors, setErrors] = useState<string | string[]>("")
+  const user = useContext(UserContext)
+  const router = useRouter()
 
   const displayErrors = () => {
     if (errors.length === 0) return
@@ -32,6 +37,28 @@ const SignupForm = () => {
     const formData = { username, password1, password2 };
     const { response, result } = await postFormData(formData, '/api/signup');
 
+    
+    // Account created successfully; get JWTs
+    try {
+      const { response, result } = await harperFetchJWTTokens(
+        username,
+        password1
+      )
+      const accessToken = result.operation_token
+      if (response.status === 200 && accessToken) {
+        authenticateUser(username, accessToken)
+      } else {
+        // Account created, but failed to get JWTs
+        // Redirect to login page
+        router.push("/login")
+      }
+    } 
+    catch (err) {
+      console.log(err)
+      setErrors("Whoops, something went wrong :(")
+    }
+
+
     // Account not created successfully
     if (response.status !== 200) {
       setErrors(result.error)
@@ -40,6 +67,10 @@ const SignupForm = () => {
 
     console.log({ response, result });
   };
+  const authenticateUser = (username: string, accessToken: string) => {
+    user.setUsername(username)
+    localStorage.setItem("access_token", accessToken)
+  }
 
   return (
     <form className="w-full sm:w-96" onSubmit={handleSubmit}>
